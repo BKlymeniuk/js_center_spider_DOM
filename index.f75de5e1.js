04ec61434caa057aa5229f70f31372d8d05031a7
Bohdan Klymeniuk <klymeniuk.bohdan@gmail.com>
2024-07-07 14:01:00 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+spider.style.position = "relative";
+wall.style.display = "flex";
+wall.style.alignItems = "center";
+wall.style.justifyContent = "center";
+
+//# sourceMappingURL=index.f75de5e1.js.map
